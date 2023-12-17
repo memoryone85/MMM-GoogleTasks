1f5250bf6ace1b90fb232e4f7315ee5b3260d1a6
Tim <tim@tg-software.com>
2023-12-17 17:31:09 +0100

    Code formatting

--- MMM-GoogleTasks.js
+++ MMM-GoogleTasks.js
@@ -1,197 +1,212 @@
 
 Module.register("MMM-GoogleTasks", {
-  // Default module config.
-  defaults: {
-    listID: "", // List ID (see authenticate.js)
-    maxResults: 10,
-    showCompleted: false, //set showCompleted and showHidden true
-    dateFormat: "MMM Do", // Format to display dates (moment.js formats)
-    updateInterval: 10000, // Time between content updates (millisconds)
-    animationSpeed: 2000, // Speed of the update animation (milliseconds)
-    tableClass: "small" // Name of the classes issued from main.css
+    // Default module config.
+    defaults: {
+        listID: "", // List ID (see authenticate.js)
+        maxResults: 10,
+        showCompleted: false, //set showCompleted and showHidden true
+        dateFormat: "MMM Do", // Format to display dates (moment.js formats)
+        updateInterval: 10000, // Time between content updates (millisconds)
+        animationSpeed: 2000, // Speed of the update animation (milliseconds)
+        tableClass: "small" // Name of the classes issued from main.css
 
-    // Pointless for a mirror, not currently implemented
-    /* 
-    dueMax: "2040-07-11T18:30:00.000Z", // RFC 3339 timestamp 
-    dueMin: "1970-07-11T18:30:00.000Z", // RFC 3339 timestamp 
-    completedMax: "2040-07-11T18:30:00.000Z", //only if showCompleted true (RFC 3339 timestamp)
-    completedMin: "1970-07-11T18:30:00.000Z", //only if showCompleted true (RFC 3339 timestamp)
-    */
-  },
+        // Pointless for a mirror, not currently implemented
+        /* 
+        dueMax: "2040-07-11T18:30:00.000Z", // RFC 3339 timestamp 
+        dueMin: "1970-07-11T18:30:00.000Z", // RFC 3339 timestamp 
+        completedMax: "2040-07-11T18:30:00.000Z", //only if showCompleted true (RFC 3339 timestamp)
+        completedMin: "1970-07-11T18:30:00.000Z", //only if showCompleted true (RFC 3339 timestamp)
+        */
+    },
 
-  // Define required scripts
-  getScripts: function () {
-    return ["moment.js"];
-  },
+    // Define required scripts
+    getScripts: function ()
+    {
+        return ["moment.js"];
+    },
 
-  // Define required scripts.
-  getStyles: function () {
-    return ["font-awesome.css", "MMM-GoogleTasks.css"];
-  },
+    // Define required scripts.
+    getStyles: function ()
+    {
+        return ["font-awesome.css", "MMM-GoogleTasks.css"];
+    },
 
-  // Define start sequence
-  start: function () {
-    Log.info("Starting module: " + this.name);
-    this.tasks;
-    this.loaded = false;
-    if (!this.config.listID) {
-      Log.log("config listID required");
-    } else {
-      this.sendSocketNotification("MODULE_READY", {});
-    }
+    // Define start sequence
+    start: function ()
+    {
+        Log.info("Starting module: " + this.name);
+        this.tasks;
+        this.loaded = false;
+        if (!this.config.listID)
+        {
+            Log.log("config listID required");
+        }
+        else
+        {
+            this.sendSocketNotification("MODULE_READY", {});
+        }
 
-    // API requies completed config settings if showCompleted
-    if (!this.config.showCompleted) {
-      // delete this.config.completedMin;
-      // delete this.config.completedMax;
-    } else {
-      this.config.showHidden = true;
-    }
-  },
+        // API requies completed config settings if showCompleted
+        if (!this.config.showCompleted)
+        {
+            // delete this.config.completedMin;
+            // delete this.config.completedMax;
+        }
+        else
+        {
+            this.config.showHidden = true;
+        }
+    },
 
-  socketNotificationReceived: function (notification, payload) {
-      var self = this;
-    if (notification === "SERVICE_READY") {
-      self.sendSocketNotification("REQUEST_UPDATE", self.config);
+    socketNotificationReceived: function (notification, payload)
+    {
+        var self = this;
+        if (notification === "SERVICE_READY") {
+            self.sendSocketNotification("REQUEST_UPDATE", self.config);
 
-      // Create repeating call to node_helper get list
-      setInterval(function () {
-        self.sendSocketNotification("REQUEST_UPDATE", self.config);
-      }, self.config.updateInterval);
+            // Create repeating call to node_helper get list
+            setInterval(function ()
+                {
+                    self.sendSocketNotification("REQUEST_UPDATE", self.config);
+                }, self.config.updateInterval);
 
-      // Check if payload id matches module id
-    } else if (
-      notification === "UPDATE_DATA" &&
-      payload.id === self.config.listID
-    ) {
-      // Handle new data
-      self.loaded = true;
-      if (payload.items) {
-          
+            // Check if payload id matches module id
+        }
+        else if (notification === "UPDATE_DATA" && payload.id === self.config.listID)
+        {
+            // Handle new data
+            self.loaded = true;
+            if (payload.items)
+            {
+                let nowDate = new Date();
+                nowDate.setHours(0);
+                nowDate.setMinutes(0);
+                nowDate.setSeconds(0);
+                let nowTime = nowDate.getTime();
+                if (payload.items
+                    .filter((task) => task.parent === undefined) // Filter tasks to only parent tasks
+                    .filter(function(task)
+                        {
+                            let taskDate = new Date(task.due);
+                            taskDate.setHours(0);
+                            taskDate.setMinutes(0);
+                            taskDate.setSeconds(0);
+                            return taskDate.getTime() <= nowTime;
+                        })
+                    .some( (item) => item.status !== "completed" ))
+                {
+                    self.sendNotification("USER_PRESENCE", true);
+                    self.didSendEmptyPresence = false;
+                }
+                else
+                {
+                    if (!self.didSendEmptyPresence)
+                    {
+                        self.sendNotification("USER_PRESENCE", true);
+                        self.didSendEmptyPresence = true;
+                    }
+                }
+                self.tasks = payload.items;
+                self.updateDom(self.config.animationSpeed);
+            }
+            else
+            {
+                if (!self.didSendEmptyPresence)
+                {
+                    self.sendNotification("USER_PRESENCE", true);
+                    self.didSendEmptyPresence = true;
+                }
+                self.tasks = null;
+                Log.info("No tasks found.");
+                self.updateDom(self.config.animationSpeed);
+            }
+        }
+    },
+
+    getDom: function ()
+    {
+        var self = this;
+
+        let wrapper = document.createElement("div");
+        wrapper.className = "container ";
+        wrapper.className += this.config.tableClass;
+
+        if (!this.tasks)
+        {
+            wrapper.innerHTML = this.loaded ? "EMPTY" : "LOADING";
+            wrapper.className = this.config.tableClass + " dimmed";
+            return wrapper;
+        }
+
         let nowDate = new Date();
         nowDate.setHours(0);
         nowDate.setMinutes(0);
         nowDate.setSeconds(0);
         let nowTime = nowDate.getTime();
-          if (payload.items
-                .filter((task) => task.parent === undefined) // Filter tasks to only parent tasks
-                .filter(function(task)
+        this.tasks = this.tasks
+            .filter((task) => task.parent === undefined) // Filter tasks to only parent tasks
+            .filter(function(task)
                 {
                     let taskDate = new Date(task.due);
                     taskDate.setHours(0);
                     taskDate.setMinutes(0);
                     taskDate.setSeconds(0);
                     return taskDate.getTime() <= nowTime;
-                })
-                .some( (item) => item.status !== "completed" ))
-          {
-            self.sendNotification("USER_PRESENCE", true);
-            self.didSendEmptyPresence = false;
-          }
-          else
-          {
-              if (!self.didSendEmptyPresence)
-              {
-                self.sendNotification("USER_PRESENCE", true);
-                self.didSendEmptyPresence = true;
-              }
-          }
-        self.tasks = payload.items;
-        self.updateDom(self.config.animationSpeed);
-      } else {
-          if (!self.didSendEmptyPresence)
-          {
-            self.sendNotification("USER_PRESENCE", true);
-            self.didSendEmptyPresence = true;
-          }
-        self.tasks = null;
-        Log.info("No tasks found.");
-        self.updateDom(self.config.animationSpeed);
-      }
+                });
+
+        // Sort attributes like they are shown in the Tasks app
+        this.tasks = this.tasks.sort((a, b) => a.due > b.due ? 1 : -1);
+
+        this.tasks.forEach((item, index) =>
+            {
+                let titleWrapper, dateWrapper, noteWrapper;
+
+                titleWrapper = document.createElement("div");
+                titleWrapper.className = "item title";
+
+                // If item is completed change icon to checkmark
+                let bullet = (item.status === "completed" ? "fa-check" : "fa-circle-thin");
+                if (item.status === "completed")
+                {
+                    titleWrapper.className += " done";
+                }
+
+                titleWrapper.innerHTML = '<i class="fa ' + bullet + '" ></i>' + item.title;
+
+                if (item.parent)
+                {
+                    titleWrapper.className = "item child";
+                }
+
+                if (item.notes)
+                {
+                    noteWrapper = document.createElement("div");
+                    noteWrapper.className = "item notes light";
+                    noteWrapper.innerHTML = item.notes.replace(/\n/g, "<br>");
+                    titleWrapper.appendChild(noteWrapper);
+                }
+
+                dateWrapper = document.createElement("div");
+                dateWrapper.className = "item date light";
+
+                if (item.due)
+                {
+                    let date = moment(item.due);
+                    dateWrapper.innerHTML = date.utc().format(this.config.dateFormat);
+                }
+
+                let thisItem = item;
+                titleWrapper.addEventListener("click", (event) =>
+                    {
+                        Log.log("Clicked");
+                        titleWrapper.style.backgroundColor = 'salmon';
+                        self.sendSocketNotification("TOGGLE_COMPLETE", {config:self.config, task:thisItem});
+                    });
+
+                wrapper.appendChild(titleWrapper);
+                wrapper.appendChild(dateWrapper);
+            });
+
+        return wrapper;
     }
-  },
-
-  getDom: function () {
-    var self = this;
-
-    let wrapper = document.createElement("div");
-    wrapper.className = "container ";
-    wrapper.className += this.config.tableClass;
-
-    if (!this.tasks) {
-      wrapper.innerHTML = this.loaded ? "EMPTY" : "LOADING";
-      wrapper.className = this.config.tableClass + " dimmed";
-      return wrapper;
-    }
-    
-    let nowDate = new Date();
-    nowDate.setHours(0);
-    nowDate.setMinutes(0);
-    nowDate.setSeconds(0);
-    let nowTime = nowDate.getTime();
-    this.tasks = this.tasks
-        .filter((task) => task.parent === undefined) // Filter tasks to only parent tasks
-        .filter(function(task)
-        {
-            let taskDate = new Date(task.due);
-            taskDate.setHours(0);
-            taskDate.setMinutes(0);
-            taskDate.setSeconds(0);
-            return taskDate.getTime() <= nowTime;
-        });
-
-    // Sort attributes like they are shown in the Tasks app
-    this.tasks = this.tasks.sort((a, b) =>
-      a.due > b.due ? 1 : -1
-    );
-
-    this.tasks.forEach((item, index) => {
-      let titleWrapper, dateWrapper, noteWrapper;
-      
-      titleWrapper = document.createElement("div");
-      titleWrapper.className = "item title";
-      
-      // If item is completed change icon to checkmark
-      let bullet = (item.status === "completed" ? "fa-check" : "fa-circle-thin");
-      if (item.status === "completed")
-      {
-          titleWrapper.className += " done";
-      }
-      
-      titleWrapper.innerHTML =
-        '<i class="fa ' + bullet + '" ></i>' + item.title;
-
-      if (item.parent) {
-        titleWrapper.className = "item child";
-      }
-
-      if (item.notes) {
-        noteWrapper = document.createElement("div");
-        noteWrapper.className = "item notes light";
-        noteWrapper.innerHTML = item.notes.replace(/\n/g, "<br>");
-        titleWrapper.appendChild(noteWrapper);
-      }
-
-      dateWrapper = document.createElement("div");
-      dateWrapper.className = "item date light";
-
-      if (item.due) {
-        let date = moment(item.due);
-        dateWrapper.innerHTML = date.utc().format(this.config.dateFormat);
-      }
-      
-      let thisItem = item;
-      titleWrapper.addEventListener("click", (event) =>
-      {
-          Log.log("Clicked");
-          titleWrapper.style.backgroundColor = 'salmon';
-          self.sendSocketNotification("TOGGLE_COMPLETE", {config:self.config, task:thisItem});
-      });
-
-      wrapper.appendChild(titleWrapper);
-      wrapper.appendChild(dateWrapper);
-    });
-
-    return wrapper;
-  }
 });
--- node_helper.js
+++ node_helper.js
@@ -5,22 +5,26 @@ const Log = require('logger');
 
 module.exports = NodeHelper.create({
 
-    start: function() {
-        
+    start: function()
+    {
         console.log("Starting node helper for: " + this.name);
 
         this.oAuth2Client;
         this.service;
     },
 
-    socketNotificationReceived: function(notification, payload) {
+    socketNotificationReceived: function(notification, payload)
+    {
         Log.log("Got notification");
 
         if (notification === "MODULE_READY")
         {
-            if(!this.service) {
+            if (!this.service)
+            {
                 this.authenticate();
-            } else {
+            }
+            else
+            {
                 // Check if tasks service is already running, avoids running authentication twice
                 console.log("TASKS SERVICE ALREADY RUNNING, DONT NEED TO AUTHENTICATE AGAIN")
                 this.sendSocketNotification("SERVICE_READY", {});
@@ -38,38 +42,51 @@ module.exports = NodeHelper.create({
         }
     },
 
-    authenticate: function() {
+    authenticate: function()
+    {
         var self = this;
 
-        fs.readFile(self.path + '/credentials.json', (err, content) => {
-            if (err) return console.log('Error loading client secret file:', err);
-            // Authorize a client with credentials, then call the Google Tasks API.
-            authorize(JSON.parse(content), self.startTasksService);
-          });
+        fs.readFile(self.path + '/credentials.json', (err, content) =>
+            {
+                if (err)
+                {
+                    return console.log('Error loading client secret file:', err);
+                }
+                
+                // Authorize a client with credentials, then call the Google Tasks API.
+                authorize(JSON.parse(content), self.startTasksService);
+            });
 
-        function authorize(credentials, callback) {
+        function authorize(credentials, callback)
+        {
             const {client_secret, client_id, redirect_uris} = credentials.installed;
-            self.oAuth2Client = new google.auth.OAuth2(
-                client_id, client_secret, redirect_uris[0]);
+            self.oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
           
             // Check if we have previously stored a token.
-            fs.readFile(self.path + '/token.json', (err, token) => {
-              if (err) return console.log('Error loading token');
-              self.oAuth2Client.setCredentials(JSON.parse(token));
-              callback(self.oAuth2Client, self);
-            });
+            fs.readFile(self.path + '/token.json', (err, token) =>
+                {
+                    if (err)
+                    {
+                        return console.log('Error loading token');
+                    }
+                    self.oAuth2Client.setCredentials(JSON.parse(token));
+                    callback(self.oAuth2Client, self);
+                });
         }
     },
 
-    startTasksService: function(auth, self) {
+    startTasksService: function(auth, self)
+    {
         self.service = google.tasks({version: 'v1', auth});
         self.sendSocketNotification("SERVICE_READY", {});
     },
 
-    getList: function(config) {
+    getList: function(config)
+    {
         var self = this;
 
-        if(!self.service) {
+        if (!self.service)
+        {
             console.log("Refresh required"); 
             return;
         }
@@ -78,37 +95,40 @@ module.exports = NodeHelper.create({
             tasklist: config.listID,
             maxResults: config.maxResults,
             showCompleted: config.showCompleted,
-            showHidden: config.showHidden,
-        }, (err, res) => {
-            if (err) return console.error('The API returned an error: ' + err);
+            showHidden: config.showHidden
+            }, (err, res) =>
+            {
+                if (err) return console.error('The API returned an error: ' + err);
 
-            // Testing
-            /* 
-            const tasksList = res.data.items;
-            console.log(tasksList);
-            if (tasksList) {
-                tasksList.forEach((task) => {
-                    console.log(task);
-                });
-            } else {
-                console.log('No tasks found.');
-            }
-             */
+                // Testing
+                /* 
+                const tasksList = res.data.items;
+                console.log(tasksList);
+                if (tasksList)
+                {
+                    tasksList.forEach((task) => { console.log(task); });
+                }
+                else
+                {
+                    console.log('No tasks found.');
+                }
+                */
 
-            var payload = {id: config.listID, items: res.data.items};
-            self.sendSocketNotification("UPDATE_DATA", payload);
-        });
+                var payload = {id: config.listID, items: res.data.items};
+                self.sendSocketNotification("UPDATE_DATA", payload);
+            });
     },
-    
+
     setComplete: function(config, item)
     {
         var self = this;
-        
-        if (!self.service) {
+
+        if (!self.service)
+        {
             console.log("Refresh required"); 
             return;
         }
-        
+
         if (item.status === "completed")
         {
             item.status = "needsAction";
@@ -117,30 +137,32 @@ module.exports = NodeHelper.create({
         {
             item.status = "completed";
         }
+
         self.service.tasks.update({
             tasklist: config.listID,
             task: item.id,
             requestBody: item,
-        }, (err, res) => {
-            if (err) return console.error('The API returned an error: ' + err);
+            }, (err, res) =>
+            {
+                if (err) return console.error('The API returned an error: ' + err);
 
-            // Testing
-            /* 
-            const tasksList = res.data.items;
-            console.log(tasksList);
-            if (tasksList) {
-                tasksList.forEach((task) => {
-                    console.log(task);
-                });
-            } else {
-                console.log('No tasks found.');
-            }
-             */
-             
-             self.getList(config);
+                // Testing
+                /* 
+                const tasksList = res.data.items;
+                console.log(tasksList);
+                if (tasksList) {
+                    tasksList.forEach((task) => {
+                        console.log(task);
+                    });
+                } else {
+                    console.log('No tasks found.');
+                }
+                */
 
-            //var payload = {id: config.listID, items: res.data.items};
-            //self.sendSocketNotification("UPDATE_DATA", payload);
-        });
+                self.getList(config);
+
+                //var payload = {id: config.listID, items: res.data.items};
+                //self.sendSocketNotification("UPDATE_DATA", payload);
+            });
     }
 });
